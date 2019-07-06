2c9133824f0e72bc440da87277b05b3642986fa0
sisimogangg <godfrey@sisimogang.co.za>
2019-07-06 18:58:12 +0200

    Completed the bounded parallelism

---
+++ digesting-a-tree/bounded-parallelism/main.go
@@ -0,0 +1,122 @@
+package main
+
+/*
+The MD5All implementation in parallel.go starts a new goroutine for each file.
+In a directory with many large files, this may allocate more memory than is available on the machine.
+*/
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"sync"
+)
+
+type result struct {
+	path string
+	sum  [md5.Size]byte
+	err  error
+}
+
+var count int = 0
+
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
+	errc := make(chan error, 1)
+	go func() {
+		defer close(paths)
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !info.Mode().IsRegular() {
+				return nil
+			}
+
+			select {
+			case paths <- path:
+			case <-done:
+				return errors.New("Walk canceled")
+			}
+
+			return nil
+		})
+
+	}()
+	return paths, errc
+}
+
+func digester(done <-chan struct{}, paths <-chan string, c chan<- result) {
+	count++
+	fmt.Println("Routine# : ", count)
+	for path := range paths {
+		fmt.Printf("Path : %s  : processed by thread : %d \n ", path, count)
+		data, err := ioutil.ReadFile(path)
+		select {
+		case c <- result{path, md5.Sum(data), err}:
+		case <-done:
+			return
+
+		}
+	}
+}
+
+// MD5All returns map of path and md5sum
+func MD5All(root string) (map[string][md5.Size]byte, error) {
+	done := make(chan struct{})
+	defer close(done)
+
+	paths, errc := walkFiles(done, root)
+
+	// Start a fixed number of goroutines to read and digest files.
+	c := make(chan result)
+	var wg sync.WaitGroup
+	const numDigesters = 20
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, paths, c)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	m := make(map[string][md5.Size]byte)
+	for r := range c {
+		if r.err != nil {
+			return nil, r.err
+		}
+		m[r.path] = r.sum
+	}
+	// Check whether the Walk failed.
+	if err := <-errc; err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func main() {
+	// Calculate the MD5 sum of all files under the specified directory,
+	// then print the results sorted by path name.
+	m, err := MD5All(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var paths []string
+	for path := range m {
+		paths = append(paths, path)
+	}
+	sort.Strings(paths)
+	for _, path := range paths {
+		fmt.Printf("%x  %s\n", m[path], path)
+	}
+}
